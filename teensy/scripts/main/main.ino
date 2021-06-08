9e8be5b66a276818b1ff4148b6bb7f72eb68b45d
kian <kian.s.sajjadi@gmail.com>
2021-06-09 01:31:31 +1000

    blah

--- teensy/scripts/main/main.ino
+++ teensy/scripts/main/main.ino
@@ -1,7 +1,4 @@
 #include <Encoder.h>
-#include <map.h>
-#include <string.h>
-
 
 double hall_resolution = 341.2;
 Encoder motor_1_encoder(14, 15);
@@ -11,43 +8,43 @@ long motor_1_current_value  = -999;
 long motor_2_current_value = -999;
 long motor_3_current_value = -999;
 long motor_1_new_value, motor_2_new_value, motor_3_new_value;
+double joint_angle;
 
 long read_motor_encoder(Encoder motor_encoder) {
   long motor_new_value;
-  new_motor_value = motor_encoder.read();
+  motor_new_value = motor_encoder.read();
   return motor_new_value;
 }
 
 double get_joint_angle(long motor_current_value) {
-  double joint_angle;
-  joint_angle = (360/hall_resolution) * motor_current_value;
+  joint_angle = ((360 / (hall_resolution * 2)) * motor_current_value) / 4;
   return joint_angle;
 }
 
-void process_encoder_data(long motor_current_value, long motor_new_value, string motor_name) {
-  map<string, long> motor_name_value_pair;
+void process_encoder_data(long motor_current_value, long motor_new_value, int motor_id) {
   if (motor_new_value != motor_current_value) {
     motor_current_value = motor_new_value;
     joint_angle = get_joint_angle(motor_current_value);
-    motor_name_value_pair[motor_name] = joint_angle;
+    Serial.print(motor_id);
+    Serial.print(",");
+    Serial.println(joint_angle);
+    delay(50);
   }
-
-  Serial.print(motor_name_value_pair)
 }
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
 }
 
 void loop() {
   motor_1_new_value = read_motor_encoder(motor_1_encoder);
-  process_encoder_data(motor_1_current_value, motor_1_new_value, "motor_1");
+  process_encoder_data(motor_1_current_value, motor_1_new_value, 1);
 
   motor_2_new_value = read_motor_encoder(motor_2_encoder);
-  process_encoder_data(motor_2_current_value, motor_2_new_value, "motor_2");
+  process_encoder_data(motor_2_current_value, motor_2_new_value, 2);
 
   motor_3_new_value = read_motor_encoder(motor_3_encoder);
-  process_encoder_data(motor_3_current_value, motor_3_new_value, "motor_3");
+  process_encoder_data(motor_3_current_value, motor_3_new_value, 3);
 
 }
